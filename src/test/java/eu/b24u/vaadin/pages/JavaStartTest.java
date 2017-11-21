0aaffb4ae1c611ad673da23eb2c588a3671bbbf1
jakub-olszewski <j.b.olszewski@gmail.com>
2017-11-21 21:10:42 +0100

    Add test selenium

--- src/test/java/eu/b24u/vaadin/pages/JavaStartTest.java
+++ src/test/java/eu/b24u/vaadin/pages/JavaStartTest.java
@@ -37,22 +37,6 @@ public class JavaStartTest {
 			// driver.close();
 		}
 
-		/*
-		 * File file = new File("resources/chromedriver.exe"); String absolutePath =
-		 * file.getAbsolutePath();
-		 * 
-		 * ClassLoader classLoader = getClass().getClassLoader(); String path =
-		 * classLoader.getResource("chromedriver.exe").getPath();
-		 * 
-		 * System.setProperty("webdriver.chrome.driver", path);
-		 * System.setProperty("selenide.browser", "Chrome");
-		 * 
-		 * GooglePage page = open("https://www.google.com/", GooglePage.class);
-		 * SearchResultsPage results = page.searchFor("selenide");
-		 * results.getResults().shouldHave(size(9)); //
-		 * results.getResult(0).shouldHave(text("Selenide: concise UI tests in Java"));
-		 * 
-		 */
 	}
 
 	private boolean firstPageContainsQAANet() {
