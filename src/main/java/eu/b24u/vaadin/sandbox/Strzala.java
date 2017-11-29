1e6d4a41b300a86cccc06821047cd7dc9b86b6d4
jakub-olszewski <j.b.olszewski@gmail.com>
2017-11-29 19:40:09 +0100

    Strzala dodanie lini

--- src/main/java/eu/b24u/vaadin/sandbox/Strzala.java
+++ src/main/java/eu/b24u/vaadin/sandbox/Strzala.java
@@ -17,7 +17,8 @@ public class Strzala extends ElementNaPlotnie {
 		Punkt c = new Punkt(x + 200, y + 50);
 		Punkt b = new Punkt(x + 50, y + 200);
 		plotno.rysujTrojkat(a, b, c, Color.RED);
-
+		Punkt d = new Punkt(x + 300, y + 300);
+		plotno.rysujLinie(a, d, Color.RED);
 	}
 
 }
