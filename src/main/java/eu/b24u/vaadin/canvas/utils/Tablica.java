d6f499ff8356395fc5f5fadea5aa91fc5009d822
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-01-03 19:12:37 +0100

    Wylosowana suma punktow

--- src/main/java/eu/b24u/vaadin/canvas/utils/Tablica.java
+++ src/main/java/eu/b24u/vaadin/canvas/utils/Tablica.java
@@ -4,26 +4,63 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
+ * tablica.java to klasa, ktora zawiera metody takie jak: dodajLiczbe
+ * losujLiczbe losujLiczbeZPrzedzialu
  * 
  * @author student
  *
  */
 public class Tablica extends ArrayList<Integer> {
-
+	/**
+	 * Konstruktor-tworzy obiekt typu tablica. To jest miejsce ktore wykonuje sie
+	 * gdy wpisujemy new Tablica()
+	 * 
+	 * 
+	 */
 	public Tablica() {
 		super();
 	}
 
+	/**
+	 * metoda z parametrem ktora dodaje liczbe do tablicy.
+	 *
+	 * @param liczba
+	 *            to liczba ktora dodajemy do tablicy.
+	 */
 	public void dodajLiczbe(int liczba) {
 		add(liczba);
 	}
 
+	/**
+	 * losuje liczbe z tablicy
+	 * 
+	 * @return dostajemy wylosowana liczbe z tablicy.
+	 */
 	public int losujLiczbe() {
 		return get(losujLiczbeZPrzedzialu(0, size() - 1));
 	}
 
+	/**
+	 * losuje liczbe z przedzialu od min do max.
+	 * 
+	 * @param min
+	 *            najmniejsza liczba calkowita jaka moze sie wylosowac.
+	 * @param max
+	 *            najwieksza liczba calkowita jaka moze sie wylosowac.
+	 * @return liczba z przedzialu od min do max
+	 */
 	private int losujLiczbeZPrzedzialu(int min, int max) {
 		return new Random().ints(min, max + 1).findFirst().getAsInt();
+	}
+
+	/**
+	 * Wypisywanie liczby w konsoli
+	 * 
+	 * @param liczba
+	 *            ktora wypisujemy
+	 */
+	public void wypisz(int liczba) {
+		System.out.println("Liczba " + liczba);
 	}
 
 	/**
@@ -32,11 +69,22 @@ public class Tablica extends ArrayList<Integer> {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Tablica tablica = new Tablica();
-		tablica.dodajLiczbe(40);
-		tablica.dodajLiczbe(60);
-		tablica.dodajLiczbe(80);
-		System.out.println(tablica.losujLiczbe());
+		Tablica tablicaPunktow = new Tablica();
+		tablicaPunktow.dodajLiczbe(40);
+		tablicaPunktow.dodajLiczbe(60);
+		tablicaPunktow.dodajLiczbe(80);
+		tablicaPunktow.dodajLiczbe(100);
+		int wylosowanaLiczba; // tworzenie zmiennej o nazwie wylosowana liczba
+		wylosowanaLiczba = tablicaPunktow.losujLiczbe();
+		// tablicaPunktow.wypisz(wylosowanaLiczba);
+		int sumaPunktow = 0;
+		sumaPunktow = wylosowanaLiczba;
+		wylosowanaLiczba = tablicaPunktow.losujLiczbe();
+		// sumaPunktow=sumaPunktow+wylosowanaLiczba;
+		sumaPunktow += wylosowanaLiczba;// zwiekszenie sumy punktow o wylosowana liczbe
+		wylosowanaLiczba = tablicaPunktow.losujLiczbe();
+		sumaPunktow += wylosowanaLiczba;
+		tablicaPunktow.wypisz(sumaPunktow);
 	}
 
 }
