485bb2da194091af5fe26dc8e1ae1687e2d8369a
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-01-27 14:05:12 +0100

    Test odczyt z pliku z graczami

--- src/test/java/eu/b24u/selenide/DataUtilsTest.java
+++ src/test/java/eu/b24u/selenide/DataUtilsTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.IOException;
 
+import org.junit.Test;
+
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 import com.google.gson.Gson;
@@ -14,11 +16,25 @@ import eu.b24u.vaadin.canvas.utils.ListaGraczy;
 
 public class DataUtilsTest {
 
-	private File getDataFile() {
+	private File pobierzPlikONazwie(String nazwaPliku) {
 		ClassLoader classLoader = getClass().getClassLoader();
-		String path = classLoader.getResource("data.json").getPath();
+		String path = classLoader.getResource(nazwaPliku).getPath();
 		File dataJson = new File(path);
 		return dataJson;
+	}
+
+	private ListaGraczy pobierzListeGraczyZPliku(File plikZGraczami) {
+		try {
+			// read
+			String result = Files.asCharSource(plikZGraczami, Charsets.UTF_8).read();
+			Gson gson = new Gson();
+			ListaGraczy listaGraczy = gson.fromJson(result, ListaGraczy.class);
+			return listaGraczy;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	// @Test
@@ -35,10 +51,10 @@ public class DataUtilsTest {
 
 		try {
 			// write
-			Files.write(listaGraczyJson.getBytes(), getDataFile());
+			Files.write(listaGraczyJson.getBytes(), pobierzPlikONazwie(""));
 
 			// read
-			String result = Files.asCharSource(getDataFile(), Charsets.UTF_8).read();
+			String result = Files.asCharSource(pobierzPlikONazwie(""), Charsets.UTF_8).read();
 			System.out.println(result);
 
 		} catch (IOException e) {
@@ -64,5 +80,18 @@ public class DataUtilsTest {
 		String result = Files.asCharSource(file, Charsets.UTF_8).read();
 
 		assertEquals(expectedValue, result);
+	}
+
+	@Test
+	public void testPobranieGraczyZPliku() {
+		// krok1 pobranie pliku
+		File pobranyPlik = pobierzPlikONazwie("data.json");
+		// krok2 wyświetlenie zawortości pliku
+		ListaGraczy listaGraczy = pobierzListeGraczyZPliku(pobranyPlik);
+		// nazwaObiektu kropka nazwaMetody
+		// Samochod samochodJasia = new Samochod();
+		// samochodJasia.otworzDrzwi();
+		listaGraczy.wypiszDoKonsoli();
+		// krok3 czy franek jest na liście
 	}
 }
