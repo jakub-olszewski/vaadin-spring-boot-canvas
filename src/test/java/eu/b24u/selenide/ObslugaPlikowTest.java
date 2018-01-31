2c71ab724b7229228064f483471bee7fe5b6b0d2
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-01-31 18:47:16 +0100

    Obsluga plikow - test zapisDoPliku

--- src/test/java/eu/b24u/selenide/ObslugaPlikowTest.java
+++ src/test/java/eu/b24u/selenide/ObslugaPlikowTest.java
@@ -1,5 +1,7 @@
 package eu.b24u.selenide;
 
+import java.io.File;
+
 import org.junit.Test;
 
 import eu.b24u.vaadin.canvas.utils.ObslugaPlikow;
@@ -10,7 +12,10 @@ public class ObslugaPlikowTest {
 	public void zapiszDoPliku() {
 		// TypObiektu nazwaObiektu = new TypObiektu();
 		ObslugaPlikow obslugaPlikow = new ObslugaPlikow();
-		String tekst;
+		// null to pusta wartosc
+		// string to napis
+		String tekst = "jakis napis";
+		File plikDoZapisu = obslugaPlikow.pobierzPlik("C:\\Users\\student\\Desktop\\dane.json");
 		// aby wywolac metode nalezy uzyc kropki po nazwieObiektu
 		obslugaPlikow.zapiszDoPliku(tekst, plikDoZapisu);
 	}
